20904857d9258a166aaf255ccdcbd404196c3ac1
Martin-Enghoy <adrianenghoy@gmail.com>
2022-08-19 03:12:35 +0800

    Fixed Sliding Navbar.

--- HTML-&-CSS/Art-Portfolio/home.js
+++ HTML-&-CSS/Art-Portfolio/home.js
@@ -4,3 +4,10 @@
 
 // quickSearch.forEach(h =>
 //     h.setAttribute('tabindex', '-1'));
+
+const navToggle = document.querySelector("#nav-menu-button");
+
+navToggle.addEventListener("click", () => {
+  //   console.log("test");
+  document.body.classList.toggle("nav-open");
+});
